5bcf9f92637d09f30fc8297fb2efb2a22003c889
intel <iftihorbekd@gmail.com>
2026-07-15 21:12:55 +0500

    Updated

--- core/dialog_manager/states.py
+++ core/dialog_manager/states.py
@@ -6,3 +6,6 @@ from enum import Enum, auto
 class DialogState(Enum):
     GREETING = auto()
     INTENT_ROUTING = auto()
+    COLLECT_SLOTS = auto()
+    CHECK_SLOTS_TOOL = auto()
+    OFFER_ALTERNATIVES = auto()
