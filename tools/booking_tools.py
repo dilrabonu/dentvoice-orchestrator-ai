72b9591e0c8c2ab8a9afe2b6509c0289ec97cbd1
intel <iftihorbekd@gmail.com>
2026-07-23 22:47:34 +0500

    Updated

--- tools/booking_tools.py
+++ tools/booking_tools.py
@@ -7,3 +7,16 @@ from observability.logging_config import get_logger
 from tools.schemas import Booking, Customer
 
 logger = get_logger(__name__)
+
+DEFAULT_DOCTOR = "MuhammadRaufxon"
+
+_SERVICES = ["konsultatsiya", "tish davolash", "tish tozalash", "ko'rik"]
+
+_PRICES = {
+    "konsultatsiya": 0,
+    "tish davolash": 0,
+    "tish tozalash": 0,
+    "ko'rik": 0,
+}
+
+_P
