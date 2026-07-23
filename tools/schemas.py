d6a1c7a4b93c8a7fb55843d29bd20ddec51be0a0
intel <iftihorbekd@gmail.com>
2026-07-23 22:40:10 +0500

    Updated

--- tools/schemas.py
+++ tools/schemas.py
@@ -4,4 +4,13 @@ from pydantic import BaseModel
 
 class Customer(BaseModel):
     name: str
+    phone: str
+
+class Booking(BaseModel):
+    booking_id: str
+    doctor: str
+    service: str
+    date: str
+    time: str
+    customer_name: str
     
