f03f607e96f4d3e53ef11eff8a5787b5ace9cd44
intel <iftihorbekd@gmail.com>
2026-07-26 17:02:34 +0500

    Updated

--- core/llm_agent/tool_schemas.py
+++ core/llm_agent/tool_schemas.py
@@ -2,6 +2,18 @@ TOOL_SCHEMAS = [
     {
         "name": "get_services",
         "description": "List available dental services offered by the clinic",
-        
+        "input_schema": {"type": "object", "properties": {}},
+
+    },
+    {
+        "name": "get_available_slots",
+        "description": "Get available appointment slots for a given date and service.",
+        "input_schema": {
+            "type": "object",
+            "properties": {
+                "date": {"type": "string", "description": "Date, e.g. 2026-07-15 or 'ertaga'"},
+                "service": 
+            }
+        }
     }
 ]
