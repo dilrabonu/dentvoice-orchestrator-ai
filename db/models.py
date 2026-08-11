f7ab130af8f94f0e0e391f093a06db04c28b9ce0
Dilrabo <iftihorbekd@gmail.com>
2026-08-11 14:34:00 +0500

    Updated

--- db/models.py
+++ db/models.py
@@ -19,3 +19,8 @@ class Customer(Base):
 
     id: Mapped[str] = mapped_column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
     tenant_id: Mapped[str] = mapped_column(String, index=True)
+    name : Mapped[str] = mapped_column(String)
+    phone: Mapped[str] = mapped_column(String, index=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+
+    
